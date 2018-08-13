cacf43cfd637dd00d5da696ce816299d7efc5758
Ari Sweedler <ari@sweedler.com>
2018-08-12 21:30:41 -0700

    Added cute stuff to the menu button
    * transform: translate(a, b) needs a comma - that's why it wasn't working
    
    * Taking advantage of the 'style.cssText += ' function coupled with
    javascript static variables to get the menu to rotate the same way with
    every click. Pretty sweet!

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,43 +1,37 @@
 /* Select DOM items */
 const menuBtn = document.querySelector('.menu-btn');
-
-let menuSingleItems = ['', '-nav', '-branding'];
-let menuListItems = ['-nav-item'];
-const menuItems = getDOMItems('.menu', menuSingleItems, menuListItems);
-console.log(menuItems);
-
+const menuItems = getDOMItems('.menu', ['', '-nav', '-branding', '-nav-item']);
 
 /* Set initial state of menu */
-let showMenu = false;
+toggleMenu.visible = false;
+toggleMenu.rotation = 0;
 
 /* add event listener to allow for action upon click */
 menuBtn.addEventListener('click', toggleMenu);
 
+/* With every call to this funciton, the menuButton will rotate an additional
+ * 180 degrees. Also, the 'close'/'show' classes will be added or removed to
+ * the proper DOM elements */
 function toggleMenu() {
-    if (!showMenu) {
+    menuBtn.style.cssText += `transform: rotate(${toggleMenu.rotation += 180}deg);`
+    
+    if (toggleMenu.visible = !toggleMenu.visible) {
         menuBtn.classList.add('close');
         menuItems.forEach(i => i.classList.add('show'));
-        showMenu = true;
     } else {
         menuBtn.classList.remove('close');
         menuItems.forEach(i => i.classList.remove('show'));
-        showMenu = false;
     }
 }
 
-function getDOMItems(base, singles, multiples) {
+/* I think this paradigm is useful */
+function getDOMItems(base, suffixes) {
     let myList = [];
 
-    for (var classSuffix of singles) {
-        let myClass = base + classSuffix;
-        myList.push(document.querySelector(myClass));
-        // console.log("Adding class ~" + myClass + "~ to menuItems");
-    }
-
-    for (var classSuffix of multiples) {
-        let myClass = base + classSuffix;
-        let myItems = document.querySelectorAll(myClass);
-        myItems.forEach(item => {myList.push(item)});
+    for (var suffix of suffixes) {
+        let myClass = base + suffix;
+        let objs = document.querySelectorAll(myClass);
+        objs.forEach(obj => {myList.push(obj)});
     }
 
     return myList
